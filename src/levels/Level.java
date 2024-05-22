d6dce1ddf5089f752548b4d53b07eafd4aea41ae
ondrasveda <sveda.ondra@seznam.cz>
2024-05-22 17:02:33 +0200

    Tried to make spikes It didnt go as planned and i probably have to think of another way of reseting the players progress if he falls

--- src/levels/Level.java
+++ src/levels/Level.java
@@ -1,8 +1,22 @@
 package levels;
 
+import main.Game;
+import object.Spike;
+import utilities.OtherMethods;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 public class Level {
 
     private int[][] levelData;
+    private ArrayList<Spike> spikes;
+    private BufferedImage image;
+    public Level(BufferedImage image) {
+        this.image = image;
+        createSpikes();
+    }
 
     public Level(int[][] levelData){
         this.levelData = levelData;
@@ -15,4 +29,13 @@ public class Level {
     public int[][] getLevelData(){
         return levelData;
     }
+    private void createSpikes() {
+        spikes = OtherMethods.GetSpikes(image);
+    }
+
+    public ArrayList<Spike> getSpikes() {
+        return spikes;
+    }
+
+
 }
